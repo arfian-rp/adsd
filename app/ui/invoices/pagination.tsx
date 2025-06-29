91b96d27d8c6f8d3920e9fe9b1c66aed73723a4d
arfian pradana <arfianpradana07@gmail.com>
2025-06-30 06:36:04 +0700

    vdfs

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -24,12 +24,8 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
-        <PaginationArrow
-          direction="left"
-          href={createPageURL(currentPage - 1)}
-          isDisabled={currentPage <= 1}
-        />
+      <div className="inline-flex">
+        <PaginationArrow direction="left" href={createPageURL(currentPage - 1)} isDisabled={currentPage <= 1} />
 
         <div className="flex -space-x-px">
           {allPages.map((page, index) => {
@@ -40,24 +36,12 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
             if (allPages.length === 1) position = 'single';
             if (page === '...') position = 'middle';
 
-            return (
-              <PaginationNumber
-                key={`${page}-${index}`}
-                href={createPageURL(page)}
-                page={page}
-                position={position}
-                isActive={currentPage === page}
-              />
-            );
+            return <PaginationNumber key={`${page}-${index}`} href={createPageURL(page)} page={page} position={position} isActive={currentPage === page} />;
           })}
         </div>
 
-        <PaginationArrow
-          direction="right"
-          href={createPageURL(currentPage + 1)}
-          isDisabled={currentPage >= totalPages}
-        />
-      </div> */}
+        <PaginationArrow direction="right" href={createPageURL(currentPage + 1)} isDisabled={currentPage >= totalPages} />
+      </div>
     </>
   );
 }
